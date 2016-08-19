6f62a9b5dcd4badb0dc7c5392a14429f61b5a92f
ehidna <fatihbulut54@gmail.com>
2016-08-19 14:51:50 +0300

    HealthBar

---
+++ Legion-TD-Project/Assets/script/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	private FighterStats parent;
+
+	[SerializeField]
+	private GameObject healthContainer;
+
+	void Start () {
+		parent = transform.GetComponentInParent<FighterStats> ();
+	}
+
+	void Update () {
+		healthContainer.transform.localScale = new Vector3(Mathf.Clamp(parent.getHealth ()/parent.getMaxHealth(), .0f, 1.0f), 1, 1);
+		transform.rotation = Quaternion.Euler (0, 0, 0);
+	}
+}
